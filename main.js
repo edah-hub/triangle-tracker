8b2b138bf24c369e1edab9b7ac1f9a0ac89e5d43
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-03-13 22:00:31 +0300

    adds a function to check if its a triangle

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 // Business Logic
 var triangle=function(sideOne,sideTwo,sideThree){
     if(isTriangle(sideOne.sideTwo,sideThree)){
-        return triangle
+        return triangleType(sideOne,sideTwo,sideThree)
+    } else{
+        return "not a triangle";
     }
+};
+
+var isTriangle=function(sideOne,sideTwo,sideThree){
+    return sideTwo=>(sideOne+sideThree) && sideTwo<=(sideThree+sideOne) && sideThree<=(sideOne+sideTwo)
 }
