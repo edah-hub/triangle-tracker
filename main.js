b0814ae4bfa78a1ed294f60f6b856691e5774e6d
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-03-13 22:16:45 +0300

    adds a function to check the type of a triangle

--- main.js
+++ main.js
@@ -9,4 +9,13 @@ var triangle=function(sideOne,sideTwo,sideThree){
 
 var isTriangle=function(sideOne,sideTwo,sideThree){
     return sideTwo=>(sideOne+sideThree) && sideTwo<=(sideThree+sideOne) && sideThree<=(sideOne+sideTwo)
+};
+
+var triangleType=function(sideOne,sideTwo,sideThree){
+    if (sideOne==sideTwo && sideTwo==sideThree){
+        return "equilateral";
+    } else if(sideOne=sideTwo || sideTwo==sideThree){
+        return "isosceles";
+    }
+    
 }
